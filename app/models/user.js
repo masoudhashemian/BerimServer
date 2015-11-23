ffbd63bb6e7edac77985c6c11a7bac365324d896
behrooz <b.m.mansouri@gmail.com>
2015-11-23 10:45:45 +0330

    Some change

--- app/models/user.js
+++ app/models/user.js
@@ -1,32 +1,53 @@
 var moment = require('moment');
 
-module.exports = function (orm, db) {
-  var Message = db.define('user', {
-    userName     : { type: 'text', required: true },
-    password      : { type: 'text', required: true},
-    phoneNumber : { type: 'number', required: true}
-  },
-  {
-    validations: {
-      userName: [
-        orm.enforce.ranges.length(1, undefined, "must be atleast 1 letter long"),
-        orm.enforce.ranges.length(undefined, 96, "cannot be longer than 96 letters")
-      ],
-      password: [
-        orm.enforce.ranges.length(1, undefined, "must be atleast 1 letter long"),
-        orm.enforce.ranges.length(undefined, 96, "cannot be longer than 96 letters")
-      ]
-    },
-    methods: {
-      serialize: function () {
-
-        return {
-          id        : this._id,
-          userName     : this.userName,
-          password      : this.password,
-		  phoneNumber    : this.phoneNumber
-        };
-      }
-    }
-  });
+module.exports = function (orm, db) 
+{
+	var Message = db.define('user', {
+		password      : { type: 'text', required: true},
+		phoneNumber : { type: 'number', required: true, unique:true}
+	},
+		
+	{   
+		 hooks: {
+					beforeCreate: function (next) {
+						obj=this;
+						Message.exists({phoneNumber: this.phoneNumber}, function (err, exists) 
+						{
+							if (exists)
+							{
+								return next(new Error("phoneNumber already exists"));
+							}
+							
+							return next();
+							
+						});
+					}
+		}  ,
+	
+	
+		validations: 
+		{
+			password: [
+				orm.enforce.security.password('lun', 'Invalid password,Password must contain number, lower and uppercase letter'),
+				orm.enforce.ranges.length(8, undefined, "username must be at least 8 letters long"),
+				orm.enforce.ranges.length(undefined, 30, "password cannot be longer than 30 letters")
+			],
+			phoneNumber:[
+				orm.enforce.ranges.length(11, 11, "Must contain 11 characters")
+			]
+		},
+		
+		methods: 
+		{
+			serialize: function () 
+			{
+				return {
+				id           : this._id,
+				password     : this.password,
+				phoneNumber  : this.phoneNumber
+				};
+			}
+		}
+	}
+	);
 };
