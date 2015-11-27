149da0a883343d0f01b66ef1fddc9d8fbb3a1880
amirveyseh <amirveyseh@gmail.com>
2015-11-27 10:24:48 +0330

    privateChat
    it's in progress

--- app/controllers/user_controller.js
+++ app/controllers/user_controller.js
@@ -19,8 +19,28 @@ module.exports ={
 									  return next(err);
 									}
 							  }
-							  
+							  console.log(user);
 							  return res.send(200, user.serialize());  	  
 						});
+					},
+					signIn : function(req, res, next){
+						var params = _.pick(req.body, 'password', 'phoneNumber');								
+						req.models.user.find(params, function (err, users) 
+						{
+							  if(err) 
+							  {
+									if(Array.isArray(err))
+									{
+									  console.log({errors: helpers.formatErrors(err) });
+									  return res.send(600, {errors: helpers.formatErrors(err) });
+									}
+									else 
+									{
+									  return next(err);
+									}
+							  }
+							  user = users[0];							  
+							  return res.send(200, user.serialize());	  
+						});						
 					}
 				};
--- app/eventHandlers/chat_handler.js
+++ app/eventHandlers/chat_handler.js
@@ -1,7 +1,51 @@
 
+var request = require('request');
+var orm     = require('orm');
+var settings = require('../../config/settings');
 
-module.exports = function(io ,socket){
+module.exports = function(io ,socket, clients){	
 	socket.on('broadcast', function(msg){	    
 		socket.broadcast.emit('broadcast', msg);
-	})
+	});
+	
+	socket.on('disconnect', function() {
+		delete clients[socket.id];
+        console.log(socket.id+' leaved!');		
+    });
+	
+	socket.on('signIn', function(data){	
+		request.post(
+			'http://localhost:'+settings.port+'/sign_in',
+			{ form: data },
+			function (error, response, body) {
+				if (!error && response.statusCode == 200) {
+				    body = JSON.parse(body);					
+					socket.userId = body.id;
+					socket.emit('signInDone', body);
+				}else{
+					data.err = body;
+					data.status = response.statusCode;	
+					socket.emit('signInFailed', data);
+				}
+			}
+		);	
+	});
+	
+	socket.on('getOnlineUsers', function(){			    
+	    users = [];		
+		numQuery = 0;
+		size = Object.keys(clients).length;
+		for(k in clients){
+			if(clients.hasOwnProperty(k)){				
+				sckt = clients[k];				
+				io.models.user.get(sckt.userId, function(err, user){	
+					numQuery = numQuery + 1;
+					users.push(user.serialize());
+					if(numQuery == size){
+						socket.emit('onlineUser', users);
+					}
+				});
+			}
+		}				
+	});
 };
